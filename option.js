e9586ebcb02fcba0d217bf55862de3831b5392e7
TalentKou <unix_c_cloud@sina.com>
2019-03-18 16:46:03 +0800

    Update option.js

--- option.js
+++ option.js
@@ -2,6 +2,15 @@ new Vue({
   el: "#app",
   data: {
     word: '(空)'
+  },
+  methods: {
+    query: function(){
+      //发送get请求
+      this.$http.get('/get').then(function(res){
+        console.log(res);
+      },function(){
+        console.log('请求失败处理');
+      });
+    }
   }
-  
 });
