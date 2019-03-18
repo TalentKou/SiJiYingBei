739536453a5b8b89dfeddfd9285566bb1e96c8df
TalentKou <unix_c_cloud@sina.com>
2019-03-18 16:00:30 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+var mysql=require("mysql");
+var express=require("express");
+var app=express();
+var connection=mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "159-357-Talent",
+  database: "WORDS"
+});
+
+connection.connect();
+
+app.get("/", function(req, res){
+  console.log("Somebody accessed");
+  
+  connection.query("SELECT * FROM jap_words", function(error, results, fields){
+    if(error){
+      console.log(error);
+      return;
+    }
+    
+    console.log(results);
+  });
+});
+
+app.listen(80);
