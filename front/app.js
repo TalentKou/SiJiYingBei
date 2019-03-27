afdc37e998f0207e4f9e66726b692b984f90ef38
TalentKou <unix_c_cloud@sina.com>
2019-03-27 12:09:45 +0800

    Update app.js

--- front/app.js
+++ front/app.js
@@ -55,9 +55,11 @@ new Vue({
 			word_type: 1,
 			word_meaning: '',
 			cur_mutant:  {
-			  mutant_id: 1,
+				mutant_id: 0,
+				mutant_type: 1,
 			  mutant_word: '',
-			  mutant_fake: ''	
+				mutant_fake: '',
+				mut_sentc_ids: []
 			},
 			cur_sentence: {
 			  sentence_id: 1,
@@ -93,6 +95,7 @@ new Vue({
 				translation: '好痛。'
 			};
 		},*/
+		//获取大量原始单词
 		getWords: function(){
 			this.$http.get('/get_words').then(function(res){
 				var m_words = res.body;
@@ -161,6 +164,74 @@ new Vue({
                 },function(){
                     console.log('请求失败处理');
                 });
+		},
+
+		/***********************************/
+    //以下为操作变形单词的方法
+		/************************************/
+
+		//添加变形单词 或者 修改变形单词
+		addMutant: function(){
+			var newW = this.newWord;
+			var mutant = newW.cur_mutant;
+			if(mutant.mutant_word.trim() == '' || 
+			mutant.mutant_fake.trim() == ''){
+				alert("请输入完整的单词(假名)意思!");
+				return;
+			}
+
+			var url = '', data = {
+				mutant_type: mutant.mutant_type,
+				mutant_word: mutant.mutant_word,
+				mutant_fake: mutant.mutant_fake,
+				mut_sentc_ids: mutant.mut_sentc_ids,
+			};
+
+			if(mutant.mutant_id == 0){
+				url = '/add_mutant';
+				data.origin_id = newW.origin_id;
+			}else{
+				url = '/update_mutant';
+				data.mutant_id = mutant.mutant_id;
+			}
+
+			this.$http.post(url, data, {emulateJSON:true}).then(
+				function(res){
+					this.resetNewWord();
+					this.getWords();
+					console.log(res.body);
+				},
+				function(res){
+					console.log(res.status);
+				});
+		},
+
+
+		/***********************************/
+    //以下为数据还原的方法
+		/************************************/
+
+		//彻底重置新数据
+		resetNewWord: function(){
+			var nWord = this.newWord;
+			var cMutant = nWord.cur_mutant;
+			var cSentence = nWord.cur_sentence;
+
+			nWord.origin_id = 0;
+			nWord.word_type = 1;
+			nWord.word_meaning = "";
+			nWord.mutant_ids.splice(0);
+			nWord.sentence_ids.splice(0);
+
+			cMutant.mutant_id = 0;
+			cMutant.mutant_type = 1;
+			cMutant.mutant_word = '';
+			cMutant.mutant_fake = '';
+			cMutant.mut_sentc_ids.splice(0);
+
+			cSentence.sentence_id = 1;
+			cSentence.content = '';
+			cSentence.translation = '';
 		}
 	}
 });
