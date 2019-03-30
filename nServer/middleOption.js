1d143c55ea191f4d9eb18b17b40e0fcaed7c882f
TalentKou <unix_c_cloud@sina.com>
2019-03-30 17:00:54 +0800

    Update middleOption.js

--- nServer/middleOption.js
+++ nServer/middleOption.js
@@ -1,168 +1,189 @@
-//数据库操作模块
-var mysql=require("mysql");
+//纯逻辑处理操作
+var dbOption = new (require("./dbOption"))();
 
-var connection=mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "159-357-Talent",
-  database: "japanese_db"
-});
+function middleOption(){
 
-connection.connect();
+    var that = this;
 
-function dbOption(){
-  
-  //插入新数据
+//关联检查
   /*参数说明:
-   *data = [table_name, //表名
-   *        [field1, field2, field3, ...], //字段名 
-   *        [value1, value2, value3, ...]]; //字段值
-   *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
+   *type, //关联者类型：0，单词；1，句子；2，语法。
+   *id, //关联者ID
+   *relType, //被关联者类型：0，单词；1，句子；2，语法。
+   *relIds, //被关联者们IDs
+   *callback = function(error, //错误信息
+                        newRelIds, //新增关联IDs
+                        dumpRelIds){}; //销毁关联IDs
    */
-  this.insert = function(data, callback){
+    function relevsCheck(type, id, relType, relIds, callback){
+        var fieldName = '',
+        newRelIds = [], //新增关联IDs
+        dumpRelIds = []; //销毁关联IDs
 
-    var sql='INSERT INTO ' + data[0] + '(' + data[1].join(',') + ') VALUES(';
-    for(var i in data[1]){
-        sql += '?,';
-    }
-    sql = sql.substr(0, sql.length-1) + ')';
+        switch(relType){
+            case 0: fieldName = 'rel_wd_ids';break;
+            case 1: fieldName = 'rel_juzi_ids';break;
+            case 2: fieldName = 'rel_grm_ids';break;
+        }
 
-    connection.query(sql, data[2],
-        function(error, result){
-            
-            callback&&callback(error, result);
-
+        //查询关联者
+        dbOption.selectById(type, id, function(error, result){
             if(error){
-                console.log('[FROM DATABASE: INSERT ERROR] - ',error.message);
+                console.log('[FROM DATABASE: relevant-' + id + ' NOT FOUND]');
+                callback(error);
                 return;
             }
-
-            console.log('[FROM DATABASE: INSERT ID: ',result.insertId +' ]');
-        });
-  };
-
-  //删除指定数据
-  /*参数说明:
-   *data = [table_name, //表名
-   *        [field1, field2, field3, ...], //查找字段名 
-   *        [value1, value2, value3, ...]]; //查找字段值
-   *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
-   */
-  this.delete = function(data, callback){
-
-    var sql='DELETE FROM ' + data[0] + ' WHERE ';
-
-    for(var i in data[1]){
-        sql += data[1][i] + '=' + data[2][i] + ' AND ';
-    }
-    sql = sql.substr(0, sql.length-5);
-
-    connection.query(sql, function(error, result){
-        callback&&callback(error, result);
-
-        if(error){
-            console.log('[FROM DATABASE: DELETE ERROR] - ',error.message);
-            return;
-        }
-
-        console.log('[FROM DATABASE: DELETE SUCCEED]');
-    });
-  };
-  
-  //修改旧数据
-  /*参数说明:
-   *data = [table_name, //表名
-   *        [field1, field2, field3, ...], //修改字段名 
-   *        [value1, value2, value3, ...], //修改字段值
-   *        [field1, field2, field3, ...], //查找字段名 
-   *        [field1, field2, field3, ...], //查找字段名
-   *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
-   */
-  this.update = function(data, callback){
-
-    var sql='UPDATE ' + data[0] + ' SET ' + data[1].join(' = ?,') + ' = ? WHERE ' + data[3].join(' = ?,') + ' = ?';
-
-    connection.query(sql, data[2].concat(data[4]),
-        function(error, result){
             
-            callback&&callback(error, result);
-
-            if(error){
-                console.log('[FROM DATABASE: UPDATE ERROR] - ',error.message);
-                return;
+            var observ = result,
+                curRelIds = observ[fieldName].split(','); //当前已关联IDs
+            for(var i in relIds){
+                if(curRelIds.indexOf(relIds[i]) < 0){
+                    newRelIds.push(relIds[i]);
+                }
+            }
+            for(var j in curRelIds){
+                if(relIds.indexOf(curRelIds[j]) < 0){
+                    dumpRelIds.push(curRelIds[j]);
+                }
             }
 
-            console.log('[FROM DATABASE: UPDATE affectedRows: ' + result.affectedRows +' ]');
+            callback(null, newRelIds, dumpRelIds);
         });
-  };
+    }
 
-  //查找指定数据
+    //检查大量数据是否存在
+    /*参数说明
+     *target，目标数组:[[word_id1, word_id2, word_id3, ...],
+     *                 [sentc_id1, sentc_id2, sentc_id3, ...],
+     *                 [gram_id1, gram_id2, gram_id3, ...]]
+     *callback = function(exists){}; 参数为一个与target类似的存在ids的数组。
+     */
+    this.checkExistc = function(target, callback){
+        var sum = target[0].length + target[1].length + target[2].length;
+        for(var i = 0; i < 3; i++){
+            for(var j = 0; j < target[i].length; j++){
+                (function(){
+                    var ii = i, jj = j;
+                    dbOption.selectById(ii, target[ii][jj], function(error, result){
+                        if(error || !result){
+                            target[ii][jj] = -1;
+                        }
+
+                        --sum;
+                        if(sum <= 0){
+                            for(var m = 0; m < 3; m++){
+                                for(var n = 0; n < target[m].length;){
+                                    if(target[m][n] == -1){
+                                        target[m].splice(n, 1);
+                                    }else{
+                                        ++n;
+                                    }
+                                }
+                            }
+                            callback(target);
+                        }
+                    });
+                })()
+            }
+        }
+        
+    };
+    
+    
+    //修改关联信息
   /*参数说明:
-   *data = [table_name, //表名 
-   *        start, //开始页数, 必须>=0，且start=0为第一页
-   *        pages, //查询页数, 必须>=0，且pages=0表示查询从start开始的所有数据
-   *        number, //每页单词数，必须>=1
-   *@useless[field1, field2, field3, ...], //查找字段名 
-   *@useless[value1, value2, value3, ...]]; //查找字段值
+   *type, //关联者类型：0，单词；1，句子；2，语法。
+   *id, //关联者ID
+   *relType, //被关联者类型：0，单词；1，句子；2，语法。
+   *relIds, //被关联者们IDs
    *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
    */
-  this.select = function(data, callback){
+  this.updateRelevs = function(type, id, relType, relIds, callback){
+    
+    //第一步：检查关联情况
+    relevsCheck(type, id, relType, relIds, function(error, newRelIds, dumpRelIds){
 
-    var sql = '';
-    if(data[1] == 0){
-        if(data[2] == 0){
-            sql = 'SELECT * FROM ' + data[0];
+        if(!error && newRelIds.length > 0){
+
+            //添加关联
+            addOrRemvRelevs(0, relType, newRelIds, type, id, function(error){
+                if(!error && dumpRelIds.length > 0){
+
+                    //移除关联
+                    addOrRemvRelevs(1, relType, dumpRelIds, type, id, function(){
+                        callback();
+                    });
+                }else{
+                    callback();
+                }
+            });
         }else{
-            sql = 'SELECT * FROM ' + data[0] + ' LIMIT 0,' + data[2]*data[3];
+            callback(error);
         }
-    }else{
-        if(data[2] == 0){
-            sql = 'SELECT * FROM ' + data[0] + ' LIMIT ' + data[1]*data[3] + ',10000';
-        }else{
-            sql = 'SELECT * FROM ' + data[0] + ' LIMIT ' + data[1]*data[3] + ',' + data[2]*data[3];
-        }
-    }
-
-    connection.query(sql, function(error, result){
-        callback&&callback(error, result);
-
-        if(error){
-            console.log('[FROM DATABASE: SELECT ERROR] - ',error.message);
-            return;
-        }
-
-        console.log('[FROM DATABASE: SELECT SUCCEED]');
     });
-  };
+};
 
-  //根据ID查找指定数据
-  /*参数说明:
-   *table_type, //表类型：0，单词；1，句子；2，语法。
-   *        id, //查询ID
-   *callback = function(error, result, tableName, fieldName){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
+//添加或移除关联
+/*参数说明:
+   *opt, //0，添加；1，移除
+   *type, //关联者类型：0，单词；1，句子；2，语法。
+   *ids, //关联者们IDs
+   *relType, //被关联者类型：0，单词；1，句子；2，语法。
+   *relId, //被关联者ID
+   *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
    */
-  this.selectById = function(table_type, id, callback){
 
-    var sql = 'SELECT * FROM ', tableName = '', fieldName = '';
-    switch(table_type){
-        case 0: sql += (tableName='word_tb') + ' WHERE ' + (fieldName='word_id');break;
-        case 1: sql += (tableName='sentence_tb') + ' WHERE ' + (fieldName='sentc_id');break;
-        case 2: sql += (tableName='grammar_tb') + ' WHERE ' + (fieldName='gram_id');break;
+   function addOrRemvRelevs(opt, type, ids, relType, relId, callback){
+    var fieldName = '';
+    relId += '';
+    switch(relType){
+        case 0: fieldName = 'rel_wd_ids';break;
+        case 1: fieldName = 'rel_juzi_ids';break;
+        case 2: fieldName = 'rel_grm_ids';break;
     }
 
-    sql += '=' + id;
+    if(ids instanceof String){
+        ids = ids.split(',');
+    }
 
-    connection.query(sql, function(error, result){
-        callback&&callback(error, result&&result[0], tableName, fieldName);
+    if(ids.length <= 0){callback&&callback();}
 
-        if(error){
-            console.log('[FROM DATABASE: SELECT ERROR] - ',error.message);
-            return;
-        }
+       //轮流查询关联者
+       for(var i in ids){
+           (function(){
+               var cId = ids[i];
+               dbOption.selectById(type, cId, function(error, result, tableName, idFieldName){
+                   if(error){
+                       console.log('[FROM DATABASE: relevant NOT FOUND]');
+                       callback&&callback(error);
+                       return;
+                    }
+    
+                    var observ = result;
+                    if(opt == 0){
+                        observ[fieldName] += ',' + relId;
+                    }else{
+                        var tempStr = ',' + observ[fieldName] + ',';
+                        var index = tempStr.indexOf(',' + relId + ',');
+                        if(index = 0){
+                            observ[fieldName] = tempStr.substr(relId.length + 2);
+                        }else if(index == (tempStr.length - relId.length - 2)){
+                            observ[fieldName] = tempStr.substr(1, tempStr.length - relId.length - 3);
+                        }else{
+                            tempStr = tempStr.split(',' + relId + ',').join(',');
+                            observ[fieldName] = tempStr.substr(1, tempStr.length - 2);
+                        }
+                    }
 
-        console.log('[FROM DATABASE: SELECT SUCCEED]');
-    });
-  };
+                    dbOption.update([tableName, [fieldName], [observ[fieldName]], [idFieldName], [cId]],
+                        function(){
+                            callback&&callback();
+                        });
+                });
+            })();
+       }
+       
+   }
 }
 
-module.exports = dbOption;
+module.exports = middleOption;
