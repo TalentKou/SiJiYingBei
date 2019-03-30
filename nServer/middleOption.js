b24b3839a7ccc7e5917b8214d8509fabbff4f778
TalentKou <unix_c_cloud@sina.com>
2019-03-30 13:58:36 +0800

    Update middleOption.js

--- nServer/middleOption.js
+++ nServer/middleOption.js
@@ -1,1 +1,122 @@
 //纯逻辑处理操作
+var dbOption = new (require("./dbOption"))();
+
+function middleOption(){
+
+    var that = this;
+
+//关联检查
+  /*参数说明:
+   *type, //关联者类型：0，单词；1，句子；2，语法。
+   *id, //关联者ID
+   *relType, //被关联者类型：0，单词；1，句子；2，语法。
+   *relIds, //被关联者们IDs
+   *callback = function(error, //错误信息
+                        newRelIds, //新增关联IDs
+                        dumpRelIds){}; //销毁关联IDs
+   */
+    function relevsCheck(type, id, relType, relIds, callback){
+        var fieldName = '';
+        switch(relType){
+            case 0: fieldName = 'rel_wd_ids';break;
+            case 1: fieldName = 'rel_juzi_ids';break;
+            case 2: fieldName = 'rel_grm_ids';break;
+        }
+
+        //查询关联者
+        dbOption.selectById(type, id, function(error, result){
+            if(error){
+                console.log('[FROM DATABASE: relevant-' + id + ' NOT FOUND]');
+                callback(error);
+                return;
+            }
+
+            var observ = result.body,
+                newRelIds = [], //新增关联IDs
+                dumpRelIds = [], //销毁关联IDs
+                curRelIds = observ[fieldName].split(','); //当前已关联IDs
+            for(var i in relIds){
+                if(curRelIds.indexOf(relIds[i]) < 0){
+                    newRelIds.push(relIds[i]);
+                }
+            }
+            for(var j in curRelIds){
+                if(relIds.indexOf(curRelIds[j]) < 0){
+                    dumpRelIds.push(curRelIds[j]);
+                }
+            }
+
+            callback(null, newRelIds, dumpRelIds);
+        });
+    }
+
+    
+    
+    //修改关联信息
+  /*参数说明:
+   *type, //关联者类型：0，单词；1，句子；2，语法。
+   *id, //关联者ID
+   *relType, //被关联者类型：0，单词；1，句子；2，语法。
+   *relIds, //被关联者们IDs
+   *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
+   */
+  this.updateRelevs = function(type, id, relType, relIds, callback){
+      
+    //第一步：检查关联情况
+    relevsCheck(type, id, relType, relIds, function(error, newRelIds, dumpRelIds){
+        //回调到调用者，让调用者改变自己的关联ID，剩下的关联交给中间操作处理。
+        callback(error);
+
+        if(newRelIds.length > 0){
+
+            //添加关联
+            addRelevs(relType, newRelIds, type, id, function(error){
+                if(!error && dumpRelIds.length > 0){
+
+                    //移除关联
+                    removeRelevs(relType, newRelIds, type, id);
+                }
+            });
+        }
+    });
+};
+
+//添加关联
+/*参数说明:
+   *type, //关联者类型：0，单词；1，句子；2，语法。
+   *ids, //关联者们IDs
+   *relType, //被关联者类型：0，单词；1，句子；2，语法。
+   *relId, //被关联者ID
+   *callback = function(error, result){}; //回调函数：参数传回为数据库模块返回的不加修改的值。
+   */
+
+   function addRelevs(type, ids, relType, relId, callback){
+    var fieldName = '';
+    switch(relType){
+        case 0: fieldName = 'rel_wd_ids';break;
+        case 1: fieldName = 'rel_juzi_ids';break;
+        case 2: fieldName = 'rel_grm_ids';break;
+    }
+       //轮流查询关联者
+       for(var i in ids){
+           (function(){
+               var cId = ids[i];
+               dbOption.selectById(type, cId, function(error, result, tableName, idFieldName){
+                   if(error){
+                       console.log('[FROM DATABASE: relevant NOT FOUND]');
+                       callback(error);
+                       return;
+                    }
+    
+                    var observ = result.body;
+                    observ[fieldName] += ',' + relId;
+
+                    dbOption.update([tableName, [fieldName], [observ[fieldName]], [idFieldName], [cId]]);
+                });
+    })();
+       }
+       
+   }
+}
+
+module.exports = middleOption;
