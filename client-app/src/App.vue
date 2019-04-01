204ca49b58b322bb22f72a513061790b03dc7b12
TalentKou <1107327585@qq.com>
2019-04-01 11:40:34 +0800

    'create'

--- client-app/src/App.vue
+++ client-app/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainFrame/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainFrame from './components/MainFrame.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainFrame
   }
 }
 </script>
---
+++ client-app/src/components/MainFrame.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main-frame">
+    <div class="top-frame"></div>
+    <table class="bottom-frame">
+        <tr>
+            <td v-bind:class="{ active: isActive == 1 }">单词</td>
+            <td v-bind:class="{ active: isActive == 2 }">句子</td>
+            <td v-bind:class="{ active: isActive == 3 }">语法</td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainFrame',
+  props: {
+    isActive: 1
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.active{
+    color: white;
+    background-color: #42b983;
+    font-size: 15px;
+}
+.bottom-frame td{
+    color: #42b983;
+    background-color: white;
+    font-size: 10px;
+}
+</style>
