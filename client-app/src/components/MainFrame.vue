b41f48cf4a2a479811b6145a5ceba5cbe8c0189b
TalentKou <1107327585@qq.com>
2019-04-01 16:28:15 +0800

    update many

--- client-app/src/components/MainFrame.vue
+++ client-app/src/components/MainFrame.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="main-frame">
-    <div class="top-frame"></div>
+    <div class="top-frame">
+      
+    </div>
     <table class="bottom-frame">
         <tr>
-            <!--td v-bind:class="{ active: isActive == 1 }">单词</td-->
-            <td class="active">单词</td>
-            <td v-bind:class="{ active: isActive == 2 }">句子</td>
-            <td v-bind:class="{ active: isActive == 3 }">语法</td>
+            <td :class="{ chosed: isActive == 1 }" @click="chooseThis(1)">单词</td>
+            <td :class="{ chosed: isActive == 2 }" @click="chooseThis(2)">句子</td>
+            <td :class="{ chosed: isActive == 3 }" @click="chooseThis(3)">语法</td>
         </tr>
     </table>
   </div>
@@ -17,23 +18,58 @@ export default {
   name: 'MainFrame',
   data: function(){
     return {
-      isActive: 0
+      isActive: 1
     };
+  },
+  methods: {
+    chooseThis: function(index){
+      this.isActive = index;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.bottom-frame td{
-    color: #42b983;
-    background-color: white;
-    font-size: 10px;
+.main-frame {
+  width: 100%;
+  height: 100%;
 }
 
-.active{
-    color: white;
-    background-color: #42b983;
-    font-size: 15px;
+.bottom-frame {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  border-collapse: collapse;
+  height: 7%;
+}
+.bottom-frame tr{
+  margin: 0;
+  padding: 0;
+  border: 0;
+  border-top: 1px solid green;
+}
+.bottom-frame td{
+  margin: 0;
+  padding: 0;
+  border: 0;
+  border-left: 1px solid green;
+  width: 33%;
+  color: #42b983;
+  background-color: white;
+  font-size: 25px;    
+}
+
+.bottom-frame td:first-child{
+  border-left: 0px;
+}
+
+.bottom-frame td.chosed{
+  color: white;
+  background-color: #42b983;
+  font-size: 20px;
 }
 </style>
